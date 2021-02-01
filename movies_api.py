570bc65d829ff0c0c8bd38c37897114ed20f04dc
jaitone <jaitoone@gmail.com>
2021-02-01 13:48:44 +0000

    change procfile

--- movies_api.py
+++ movies_api.py
@@ -1,1 +1,24 @@
 from app import app
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from app.routes import app
+from app import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+# migrations
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def create_db():
+    """Creates the db tables."""
+    db.create_all()
+
+
+if __name__ == '__main__':
+    # manager.add_option("--dev", required=False, defaut=config.)
+    manager.run()
