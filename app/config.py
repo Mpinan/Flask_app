149d5e441400e5f8484a99d50ff7f932c5e4a324
jaitone <jaitoone@gmail.com>
2021-02-01 14:53:23 +0000

    updated procfile

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-    DATABASE = os.getenv('DATABASE')
-    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
-    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    # DATABASE = os.getenv('DATABASE')
+    # DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+    # DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DATABASE_URL = "postgres://igcjukkymuicfq:3e1c8c7eb7753d59cd3f7362697ed5d018f49cf72b4c2640c287499ec0fa3612@ec2-52-70-135-246.compute-1.amazonaws.com:5432/dad3uilljjvvg"
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
